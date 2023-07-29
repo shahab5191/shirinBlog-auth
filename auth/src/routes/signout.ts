cd788108984dc08879098979b84a451c76914148
shahab <oveysi.shahab@gmail.com>
2023-07-29 22:22:13 +0330

    changing signout function

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,8 +3,7 @@ import express, { type Request, type Response, type NextFunction } from 'express
 const router = express.Router()
 
 router.get('/api/users/signout', (req: Request, res: Response, next: NextFunction) => {
-  req.session = {}
-  res.status(200).send({ message: 'signed out successfuly' })
+  res.clearCookie('session').status(200).send({ message: 'signed out successfuly' })
 })
 
 export { router as signoutRouter }
